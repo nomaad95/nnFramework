c56df5752ad17e68a50b6a36f96e10f0691e4786
Camille <camille.bamboute@hotmail.fr>
2021-04-08 00:17:41 +0200

    adding dataset and nn class

---
+++ nnClass.py
@@ -0,0 +1,38 @@
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+nnfs.init()
+np.random.seed(0)
+
+
+class Layer_Dense:
+
+
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10*np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+
+class Activation_ReLu:
+    def forward(self,inputs):
+        self.output = np.maximum(0,inputs)
+
+class Activation_Softmax:
+	def forward(self, input):
+		exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+		probabilites = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+		self.output = probabilites
+
+
+layer1 = Layer_Dense(2,5)
+
+activation1 = Activation_ReLu()
+#layer2 = Layer_Dense(5,2)
+
+layer1.forward(X)
+#layer2.forward(layer1.output)
+activation1.forward(layer1.output)
+print(activation1.output)
