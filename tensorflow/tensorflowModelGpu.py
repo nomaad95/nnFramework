dbdb116ecf91a5460d4a764c04c4e06f5b83665b
Camille <camille.bamboute@hotmail.fr>
2021-12-20 23:58:27 +0100

    implementing loss

--- tensorflow/tensorflowModelGpu.py
+++ tensorflow/tensorflowModelGpu.py
@@ -46,6 +46,25 @@ def softmax(inputs):
         return tf.transpose(probabilities)
 
 
+def forwardLoss(y_pred, y_true):
+    samples = len(y_pred)
+    y_pred_clipped = tf.clip_by_value(y_pred, 1e-7, 1 - 1e-7)
+    if len(y_true.shape) == 1:
+        correct_confidences = y_pred_clipped[range(samples), y_true]
+    else:
+        print("error shape")
+    negative_log_likelyhoods = -tf.math.log(correct_confidences)
+    print("negative likelyhoods", negative_log_likelyhoods)
+    return negative_log_likelyhoods
+
+def loss(output, y):
+    sample_losses = forwardLoss(output,y)
+    print("sample losses", sample_losses)
+    data_loss = tf.math.reduce_mean(sample_losses)
+    return data_loss
+
+
+
 X, y = spiral_data(samples=100000, classes=3)
 print(X)
 
@@ -55,8 +74,18 @@ start = dt.datetime.now()
 forwardDense1 = forward1(X, dense1[0], dense1[1])
 reluTensor = relu(forwardDense1)
 softmaxTensor = softmax(reluTensor)
+loss = loss(softmaxTensor, y)
+print("Loss", loss)
 end = dt.datetime.now()
 
 print("exp_values", softmaxTensor)
 
 print("execution time:", end - start)
+
+print("#")
+print("#")
+print("#")
+print("#")
+print("#")
+print("#")
+print("#")
